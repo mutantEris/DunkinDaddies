2d4144ff041e70f6302d9a4f2b7b1a18045ce8af
meddizzle316 <meddizzle316@gmail.com>
2024-04-23 20:16:07 -0500

    adding angry bird slingshot mouse

--- Dunkin' Daddies/Assets/Scripts/Slingshot.cs
+++ Dunkin' Daddies/Assets/Scripts/Slingshot.cs
@@ -11,16 +11,40 @@ public class Slingshot : MonoBehaviour
     public Transform center;
     public Transform idlePosition;
 
+    public Vector3 currentPosition;
+
+    public float bottomBoundary;
+
+    public float maxLength;
+
     bool isMouseDown;
+
+    public GameObject birdPrefab;
+
+    public float birdPositionOffset = -0.45f;
+
+    Rigidbody2D bird;
+    Collider2D birdCollider;
+
+
+    public float force;
     void Start()
     {
         lineRenderers[0].positionCount = 2;
         lineRenderers[1].positionCount = 2;
         lineRenderers[0].SetPosition(0, stripPositions[0].position);
         lineRenderers[1].SetPosition(0, stripPositions[1].position);
-
+        CreateBird();
     }
+    void CreateBird(){
+        bird = Instantiate(birdPrefab).GetComponent<Rigidbody2D>();
+        birdCollider = bird.GetComponent<Collider2D>();
+        birdCollider.enabled = false;
 
+        bird.isKinematic = true;
+
+        ResetStrips();
+    }
     
     void Update()
     {
@@ -29,10 +53,16 @@ public class Slingshot : MonoBehaviour
             Vector3 mousePosition = Input.mousePosition;
             mousePosition.z = 10;
 
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            currentPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            currentPosition = center.position + Vector3.ClampMagnitude(currentPosition - center.position, maxLength);
 
-            SetStrips(mousePosition);
-            Debug.Log("if statement triggered");
+            currentPosition = ClampBoundary(currentPosition);
+            
+            SetStrips(currentPosition);
+            
+            if (birdCollider) {
+                birdCollider.enabled = true;
+            }
         }
         else
         {
@@ -44,19 +74,44 @@ public class Slingshot : MonoBehaviour
         isMouseDown = true;
     }
 
+    void Shoot(){
+        
+        bird.isKinematic = false;
+        Vector3 birdForce = (currentPosition - center.position) * force * -1;
+        bird.velocity = birdForce;
+
+        bird = null;
+        birdCollider = null;
+        Invoke("CreateBird", 2);
+    }
     private void OnMouseUp() 
     {
         isMouseDown = false;
+        Shoot();
     }
 
     void ResetStrips()
     {
-        SetStrips(idlePosition.position);
+        currentPosition = idlePosition.position;
+        SetStrips(currentPosition);
     }
 
     void SetStrips(Vector3 position)
     {
         lineRenderers[0].SetPosition(1, position);
         lineRenderers[1].SetPosition(1, position);
+        
+        if (bird){
+            Vector3 dir = position - center.position;
+            bird.transform.position = position + dir.normalized * birdPositionOffset;
+            bird.transform.right = -dir.normalized;
+        }
+
+       
+    }   
+
+    Vector3 ClampBoundary(Vector3 vector) {
+        vector.y = Mathf.Clamp(vector.y, bottomBoundary, 1000);
+        return vector;
     }
 }
